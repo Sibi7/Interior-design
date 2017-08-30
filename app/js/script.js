7978133f100e27c58a150b8d039adb95b148c7c1
Sibi7 <Sibi_7@mail.ru>
2017-08-30 19:45:07 +0300

    fix bug

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("#my-menu").mmenu({
-        // options
-    }, {
-        // configuration
-        ClassNames: {
-            Selected: "active"
-        }
+    $(function () {
+        $('#my-menu').mmenu({
+            extensions: ['fx-menu-slide', 'shadow-page', 'shadow-panels', 'pagedim-black', 'theme-black'],
+            iconPanels: true,
+            counters: true,
+            keyboardNavigation: {
+                enable: true,
+                enhance: true
+            },
+            navbar: {
+                title: 'Меню',
+                content: ['breadcrumbs', 'close']
+            },
+            offCanvas: {
+                position: 'right'
+            },
+            navbars: [
+                {
+                    position: 'top',
+                    content: ['breadcrumbs', 'close']
+                }
+            ]
+        });
     });
 });
 
